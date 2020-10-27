0c6f1686176ecc0afd1af4d0a123c7cd72784736
Marius <mt.cristea@gmail.com>
2020-10-27 09:00:35 +0100

    Update Popup.js

--- Popup.js
+++ Popup.js
@@ -11,26 +11,58 @@ for (var i = 0; i < nums.length; i++) {
   nums[i].addEventListener("click", function typechar() {
     if(catchValue.includes("=") === false){    
       theDiv.innerHTML= theDiv.innerHTML + catchValue;      
+    }else if(catchValue.includes("null") === false){
+      theDiv.innerHTML= theDiv.innerHTML + catchValue;      
     }else{
       theDiv.innerHTML= theDiv.innerHTML
     }  
-  }, false);
+  }, false);   
+  
 }
 
 eql.onclick = function(){  
   let content = theDiv.innerHTML;
-  if(content.includes("x")){
-    let elems = content.split("x");   
-    theDiv.innerHTML = elems.reduce(function(a, b) { return parseInt(a) * parseInt(b); });      
+  if(content.includes("?")){
+    let elems = content.split("?");     
+    theDiv.innerHTML = elems.reduce(
+      function(a, b) { 
+      if(a===null || b===null|| a==="" || b==="" || a.match(/^[a-zA-Z]+$/) || b.match(/^[a-zA-Z]+$/)){
+          return document.getElementById("result").innerHTML          
+        }else{
+          return parseFloat(a) * parseFloat(b);}
+         });
   }else if(content.includes("+")){
     let elems = content.split("+")   
-    theDiv.innerHTML = elems.reduce(function(a, b) { return parseInt(a) + parseInt(b); });     
+    theDiv.innerHTML = elems.reduce(
+      function(a, b) { 
+        if(a===null || b===null|| a==="" || b==="" || a.match(/^[a-zA-Z]+$/) || b.match(/^[a-zA-Z]+$/)){
+          return document.getElementById("result").innerHTML          
+        }else{
+          return parseFloat(a) + parseFloat(b);}
+         });    
   }else if(content.includes("-")){
     let elems = content.split("-");      
-      theDiv.innerHTML =  elems.reduce(function(a, b) { return parseInt(a) - parseInt(b); });      
-   }else if(content.includes("/")){
-    let elems = content.split("/");      
-      theDiv.innerHTML =  elems.reduce(function(a, b) { return parseInt(a) / parseInt(b); });       
+      theDiv.innerHTML =  elems.reduce(
+        function(a, b) { 
+          if(a===null || b===null|| a==="" || b==="" || a.match(/^[a-zA-Z]+$/) || b.match(/^[a-zA-Z]+$/)){
+          return document.getElementById("result").innerHTML          
+        }else{
+          return parseFloat(a) - parseFloat(b);}
+         });      
+   }else if(content.includes("?")){
+    let elems = content.split("?");      
+      theDiv.innerHTML =  elems.reduce(
+        function(a, b) {
+          if(parseFloat(b) === 0){
+            return document.getElementById("result").innerHTML = "Cannot divide by zero"
+          }else{            
+            if(a===null || b===null|| a==="" || b==="" || a.match(/^[a-zA-Z]+$/) || b.match(/^[a-zA-Z]+$/)){
+          return document.getElementById("result").innerHTML          
+        }else{
+          return parseFloat(a) / parseFloat(b);}
+          }
+        
+         });       
    }
 }
 
@@ -39,6 +71,7 @@ clearall.onclick = function(){
 }
 
 deleteone.onclick = function(){
-  theDiv.innerHTML=theDiv.innerHTML.slice(0, -1);    
+  theDiv.innerHTML=theDiv.innerHTML.slice(0, -1);   
 }
+
 };
